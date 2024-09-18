4c31486f2a92fb5e74e45ad9ed1b314c56a57181
RichardWagenknecht <richardwagenknecht@icloud.com>
2024-09-18 14:17:37 -0300

    Metodo Conquistar Territorio

--- Conquista Territorio/Assets/GameManager.cs
+++ Conquista Territorio/Assets/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
 
     public float spacing;
 
+    int blocosConquistados;
+
     private GameObject[,] bloco;
 
     private void Awake()
@@ -47,6 +49,32 @@ public class GameManager : MonoBehaviour
         Camera.main.transform.position = new Vector3 (((linhas - 1)*spacing)/2, ((linhas - 1) * spacing) /2, -10);
         Camera.main.orthographicSize = (colunas * linhas) / 4;
     }
+    void ConquistarTerritorio()
+    {
+        blocosConquistados++;
 
+        if (blocosConquistados == linhas * colunas)
+        {
+            int territorioPlayer1 = 0, territorioPlayer2 = 0;
+            int dono = Bloco.instance.PegarJogadorDono();
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (dono == 1)
+                    {
+                        territorioPlayer1++;
+                    }
+                    else
+                    {
+                        territorioPlayer2++;
+                    }
+                }
+            }
+            FimDeJogo(territorioPlayer1, territorioPlayer2);
+        }
+
+    }
 
 }
