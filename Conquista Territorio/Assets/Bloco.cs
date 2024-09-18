0294667756a462256ced394776dd1b297e910d7d
ipysk <sarahjuliaantunesv@gmail.com>
2024-09-18 13:38:47 -0300

    Implementar o Método para Alterar o Estado de Conquista:

--- Conquista Territorio/Assets/Bloco.cs
+++ Conquista Territorio/Assets/Bloco.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Bloco : MonoBehaviour
 
 
 {
-  public static Bloco instance; 
   
-    private bool blocoConquista;
+    private bool conquistado = false;
     private SpriteRenderer spriteRenderer;
     private int jogadorDono;
 
     private void Awake()
     {
-        instance = this;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = Color.white;
-
+        AtualizarCor(Color.white);
     }
+
+    public void AlterarConquistas(bool jogador1, Color corDoJogador)
+    {
+        conquistado=true;
+        AtualizarCor(corDoJogador);
+    
+        if (jogador1)
+        {
+            jogadorDono = 1;
+            GameManager.Instance.NotificarConquista(1);
+
+        }
+    }
+    
 
 
 }
